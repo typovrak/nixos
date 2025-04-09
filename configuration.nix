325a5c1654ec4cfb6b12e11750e7ef9e098d38a9
typovrak <typovrak@gmail.com>
2025-04-09 09:55:17 +0200

    polybar added

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@ let
 	nixos-bash = fetchGit {
 		url = "https://github.com/typovrak/nixos-bash.git";
 		ref = "main";
-		rev = "f7817282a2a03301d9601b8cddc3c31994f457a9";
+		rev = "dc647fb656c7329b2c7bbff8b9379ddfaab272ce";
 	};
 	nixos-ssh = fetchGit {
 		url = "https://github.com/typovrak/nixos-ssh.git";
@@ -24,12 +24,32 @@ let
 	nixos-git = fetchGit {
 		url = "https://github.com/typovrak/nixos-git.git";
 		ref = "main";
-		rev = "827ceec13bbd0a5c4ca3fbf494e9f2db31df8c48";
+		rev = "5934ed0e49af7f6e03707a19a0a443bd619cfc81";
 	};
 	nixos-flatpak = fetchGit {
 		url = "https://github.com/typovrak/nixos-flatpak.git";
 		ref = "main";
-		rev = "fc3cca6c395661609b408794e251e26fdb61382a";
+		rev = "8bd077cb8ee744d5b20938ff0a2fa0318bd0a4fb";
+	};
+	nixos-fonts = fetchGit {
+		url = "https://github.com/typovrak/nixos-fonts.git";
+		ref = "main";
+		rev = "72e1873ae4c618435691418bafb7c6fe587493bb";
+	};
+	nixos-gtk = fetchGit {
+		url = "https://github.com/typovrak/nixos-gtk.git";
+		ref = "main";
+		rev = "5e7741eefa3c4b286a2e3dddcc49f64c23d727b1";
+	};
+	nixos-alacritty = fetchGit {
+		url = "https://github.com/typovrak/nixos-alacritty.git";
+		ref = "main";
+		rev = "07941de4afd240179af2cef195b4022b5c8cb257";
+	};
+	nixos-polybar = fetchGit {
+		url = "https://github.com/typovrak/nixos-polybar.git";
+		ref = "main";
+		rev = "705383143f1b49bf21c8ce377a768a753ebc4aee";
 	};
 in {
 	imports = [
@@ -40,28 +60,18 @@ in {
 		(import "${nixos-ssh}/configuration.nix")
 		(import "${nixos-git}/configuration.nix")
 		(import "${nixos-flatpak}/configuration.nix")
+		(import "${nixos-fonts}/configuration.nix")
+		(import "${nixos-gtk}/configuration.nix")
+		(import "${nixos-alacritty}/configuration.nix")
+		(import "${nixos-polybar}/configuration.nix")
 	];
 
 	system = {
-		# TODO: add stateVersion on every package
 		# TODO: add beautiful readme.md for every package
 		# TODO: replace flathub apps by nix apps perhaps? and install vscode
+		# TODO: mettre les bons shebang partout
+		# TODO: mettre les bons droits à chaque dossier créé par root
 		stateVersion = "24.11";
-		activationScripts = {
-			dotConfig = ''
-				ln -sFf /home/typovrak/nixos-config/.config /home/typovrak/.config
-			'';
-			gtk = ''
-				mkdir -p /home/typovrak/.config/gtk-3.0
-				cat > /home/typovrak/.config/gtk-3.0/settings.ini << EOF
-[Settings]
-gtk-theme-name=Adwaita-dark
-gtk-application-prefer-dark-theme=1
-EOF
-				chown typovrak:users /home/typovrak/.config/gtk-3.0/settings.ini
-				chmod 644 /home/typovrak/.config/gtk-3.0/settings.ini
-			'';
-		};
 	};
 
 	boot.loader = {
@@ -108,10 +118,10 @@ EOF
 			chromium firefox
 			docker docker-compose
 			vim neovim tmux
-			gdu fzf bat htop btop gh
+			gdu fzf bat htop btop
 			rpi-imager filezilla gedit screenkey
-			alacritty ghostty maim xclip copyq pavucontrol nautilus gnome-disk-utility brightnessctl
-			neofetch dmenu polybar feh lxappearance sddm gnome-themes-extra yazi cava
+			ghostty maim xclip copyq pavucontrol nautilus gnome-disk-utility
+			neofetch dmenu feh sddm yazi cava
 			wireplumber pamixer helvum
 		];
 		etc = {
@@ -122,15 +132,6 @@ EOF
 				sha256 = "sha256-63suKn9oC6fQDrjnSJlG9EW/EFHBh39nfUXWJbRoxgI=";
 			};
 		};
-	};
-
-	fonts = {
-		enableDefaultPackages = true;
-		fontconfig.enable = true;
-		packages = with pkgs; [
-			noto-fonts-emoji
-			(nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-		];
 	};
 
 	hardware.pulseaudio.enable = false;
@@ -162,9 +163,5 @@ EOF
 			wireplumber.enable = true;
 		};
 		printing.enable = true;
-	};
-
-	programs = {
-		dconf.enable = true;
 	};
 }
