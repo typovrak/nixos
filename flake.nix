63e871a24def41d7861811c7dc1f14cada713524
gignsky <gig@gignsky.com>
2025-05-26 14:26:56 -0400

    removed unneeded inputs

--- flake.nix
+++ flake.nix
@@ -6,14 +6,6 @@
       url = "github:hercules-ci/flake-parts";
     };
     systems.url = "github:nix-systems/default";
-    rust-flake = {
-      url = "github:juspay/rust-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Dev tools
-    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake/f6ce9481df9aec739e4e06b67492401a5bb4f0b1";
-    cargo-doc-live.url = "github:srid/cargo-doc-live/b09d5d258d2498829e03014931fc19aed499b86f";
 
     git-hooks = {
       url = "github:cachix/git-hooks.nix";
